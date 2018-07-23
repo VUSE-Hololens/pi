11d7da7ad428f4f5ab5c7bfa4ad173e9ab2dbc6e
hbristol <henry.bristol@vanderbilt.edu>
2018-07-23 16:57:05 -0500

    .

--- SenteraClass/camtest-customised/SenteraDouble4k.h
+++ SenteraClass/camtest-customised/SenteraDouble4k.h
@@ -60,8 +60,8 @@ private:
 
 	int s_send, s_rec;														// Sending and Receiving Sockets
 
-	static std::string server_ipaddr;										// Default IP of camera - was: char server_ipaddr[80]
-	static std::string local_ipaddr;										// Default local IP
+	static const std::string server_ipaddr;									// Default IP of camera - was: char server_ipaddr[80]
+	static const std::string local_ipaddr;									// Default local IP
 	uint16_t cameraPort;													// Default port of camera
 	uint16_t localPort;														// Default local port for receiving
 
