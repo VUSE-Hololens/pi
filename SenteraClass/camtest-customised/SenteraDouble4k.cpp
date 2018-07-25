c964eef1ca01251fbcf5822eebd15e88ca91345d
hbristol <henry.bristol@vanderbilt.edu>
2018-07-25 15:30:24 -0500

    .

--- SenteraClass/camtest-customised/SenteraDouble4k.cpp
+++ SenteraClass/camtest-customised/SenteraDouble4k.cpp
@@ -492,11 +492,11 @@ std::string SenteraDouble4k::makeUrlPath(uint8_t *filename) {
 	outStr += server_ipaddr;
 	outStr += ":";
 	outStr += "8080";
-	/* outStr += "/cur_session?path=/";
+	outStr += "/cur_session?path=/";
 	for (int i = 0; i < 48; i++) { // filename array size 48
 		outStr += (const char)filename[i];
-	} */
-	outStr += "/last_img?lnk=li1&camera=2";
+	}
+	// outStr += "/last_img?lnk=li1&camera=2";
 	return outStr;
 }
 
