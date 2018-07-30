2f71edb2a5fa22f61f6d85ad6b3476d7d3e71c0f
hbristol <henry.bristol@vanderbilt.edu>
2018-07-30 10:24:02 -0500

    cascading sentera data transmission

---
+++ CascadingSentera/HTTPDownloader.h
@@ -0,0 +1,66 @@
+#pragma once
+/**
+* HTTPDownloader.hpp
+*
+* A simple C++ wrapper for the libcurl easy API.
+*
+* Written by Uli Köhler (techoverflow.net)
+* Published under CC0 1.0 Universal (public domain)
+*/
+#ifndef HTTPDOWNLOADER_HPP
+#define HTTPDOWNLOADER_HPP
+#include <string>
+#include "curl/curl.h"
+#include "curl/easy.h"
+#include "curl/curlbuild.h"
+#include <sstream>
+#include <iostream>
+
+
+/**
+* A non-threadsafe simple libcURL-easy based HTTP downloader
+*/
+class HTTPDownloader {
+	//variables
+private:
+	void* curl;
+	//methods
+public:
+	HTTPDownloader() {
+		curl = curl_easy_init();
+	};
+	~HTTPDownloader() {
+		curl_easy_cleanup(curl);
+	};
+	/**
+	* Download a file using HTTP GET and store in a std::string
+	* @param url The URL to download
+	* @return The download result
+	*/
+	std::string download(const std::string& url) {
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		/* example.com is redirected, so we tell libcurl to follow redirection */
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); //Prevent "longjmp causes uninitialized stack frame" bug
+		curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
+		std::stringstream out;
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+		/* Perform the request, res will get the return code */
+		CURLcode res = curl_easy_perform(curl);
+		/* Check for errors */
+		if (res != CURLE_OK) {
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(res));
+		}
+		return out.str();
+	};
+
+private:
+	static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
+		std::string data((const char*)ptr, (size_t)size * nmemb);
+		*((std::stringstream*)stream) << data; // << endl;
+		return size * nmemb;
+	}
+};
+#endif  /* HTTPDOWNLOADER_HPP */
---
+++ CascadingSentera/SensorDataTypes.h
@@ -0,0 +1,96 @@
+#pragma once
+
+// vector 3
+struct Vector3 {
+	float x, y, z;
+
+	// default constructor
+	Vector3() {
+		x = y = z = 0;
+	}
+
+	Vector3(float _x, float _y, float _z) {
+		x = _x; 
+		y = _y;
+		z = _z;
+	}
+
+	bool equals(Vector3 & rhs) {
+		return (x == rhs.x && y == rhs.y && z == rhs.z);
+	}
+};
+
+struct Vector3Int {
+	int x, y, z;
+
+	// default constructor
+	Vector3Int() {
+		x = y = z = 0;
+	}
+
+	Vector3Int(int _x, int _y, int _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	bool equals(Vector3Int & rhs) {
+		return (x == rhs.x && y == rhs.y && z == rhs.z);
+	}
+};
+
+// positional transform
+struct Transform {
+	Vector3 position, eulerAngle;
+};
+
+// single frame of recorded sensor data
+struct Frame {
+	uint8_t *pixels;
+	int height, width, bands;
+	float FOVx, FOVy;
+
+	// default constructor
+	Frame() {
+		pixels = nullptr;
+		height = width = bands = 1;
+		FOVx = FOVy = 0;
+	}
+
+	// constructor: blank frame
+	Frame(int _height, int _width, int _bands, float _FOVx, float _FOVy)
+		: height(_height), width(_width), bands(_bands), FOVx(_FOVx), FOVy(_FOVy)
+	{
+		pixels = new uint8_t[height*width*bands];
+	}
+
+	// copy constructor: deep copy
+	Frame(const Frame & rhs)
+		: height(rhs.height), width(rhs.width), bands(rhs.bands), FOVx(rhs.FOVx), FOVy(rhs.FOVy)
+	{
+		pixels = new uint8_t[height*width*bands];
+		for (int i = 0; i < height*width*bands; i++) {
+			pixels[i] = rhs.pixels[i];
+		}
+	}
+
+	// assignment op: deep copy
+	Frame& operator=(const Frame &rhs) {
+		Frame tmp(rhs);
+		std::swap(tmp, *this);
+	}
+
+	~Frame() {
+		clear();
+	}
+
+	// accessor
+	uint8_t get(int i, int j, int k) {
+		int index = i + j * width + k * (height * width);
+		return pixels[index];
+	}
+
+	void clear() {
+		delete[] pixels;
+	}
+};
