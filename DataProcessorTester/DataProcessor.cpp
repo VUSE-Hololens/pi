060655b443b9c75030ec083a6cacfd20f578f282
hbristol <henry.bristol@vanderbilt.edu>
2018-07-31 18:13:58 -0500

    added dataprocessortester

--- DataProcessorTester/DataProcessor.cpp
+++ DataProcessorTester/DataProcessor.cpp
@@ -26,7 +26,7 @@ int main() {
 	printf("NDVI Array: \n");
 	for (int j = 0; j < height; j++) {
 		for (int i = 0; i < width; i++) {
-			printf("%d ", ndvibuf[i * width*j]);
+			printf("%d ", ndvibuf[i + width*j]);
 		}
 		printf(";\n");
 	}
