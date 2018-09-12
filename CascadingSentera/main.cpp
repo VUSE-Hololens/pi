2b02582f951efa40e208a5cbfe436b6f5a123968
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-09-12 12:38:28 -0500

    another another

--- CascadingSentera/main.cpp
+++ CascadingSentera/main.cpp
@@ -8,7 +8,7 @@ int main() {
 	// get time 
 	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 	std::tm* localtime = std::localtime((const time_t*)&now);
-	char *timeBuf[100];
+	char timeBuf[100];
 	strftime(timeBuf, 100, "%d-%m-%Y %H:%M:%S", localtime);
 
 	fprintf(stderr, "\n\n\n\n-----------------------------------------------------------------------------------------\nNDVI Configuration: new host session begun (%s)\n\n",
