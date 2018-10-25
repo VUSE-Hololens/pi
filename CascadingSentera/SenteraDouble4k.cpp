7cc70dd7898f1c3fb5f567f0f4fad6d7f4bcae2e
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-10-25 00:39:38 -0500

    Still trying for EV

--- CascadingSentera/SenteraDouble4k.cpp
+++ CascadingSentera/SenteraDouble4k.cpp
@@ -624,7 +624,7 @@ int SenteraDouble4k::processImage(int cam) {
 	// get relevant exif data
 	easyexif::EXIFInfo result;
 	result.parseFrom(compressedImg, compressedImgLength);
-	fprintf(stderr, "Parsed jpg EXIF data (%s): ISO: %d, EV: %d\n", outname.c_str(), result.ISOSpeedRatings, (unsigned)(1.0 / result.ExposureTime));
+	fprintf(stderr, "Parsed jpg EXIF data (%s): ISO: %d, EV: %f\n", outname.c_str(), result.ISOSpeedRatings, (unsigned)result.ExposureTime);
 
 	// debug
 	//std::cout << "Successfully decompressed .jpg\n";
@@ -634,7 +634,7 @@ int SenteraDouble4k::processImage(int cam) {
 	sensor_data[cam - 1].height = height;
 	sensor_data[cam - 1].bands = channels;
 	sensor_data[cam - 1].iso = result.ISOSpeedRatings;
-	sensor_data[cam - 1].ev = (unsigned)(1.0 / result.ExposureTime);
+	sensor_data[cam - 1].ev = (unsigned)result.ExposureTime;
 	updated[cam - 1] = true;
 	return 0;
 }
