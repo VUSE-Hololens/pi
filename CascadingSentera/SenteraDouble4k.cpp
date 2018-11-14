25d1cdb265d538bbd171a20c73c7d3468fe34488
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-11-14 17:07:41 -0600

    debug mode working actually hopefully

--- CascadingSentera/SenteraDouble4k.cpp
+++ CascadingSentera/SenteraDouble4k.cpp
@@ -163,7 +163,7 @@ int SenteraDouble4k::sessionListener_DEBUG() {
 		sendNDVI(jpg_quality);
 
 		// sleep
-		std::this_thread::sleep_for(std::chrono::seconds(1))
+		std::this_thread::sleep_for(std::chrono::seconds(1);
 	}
 }
 
